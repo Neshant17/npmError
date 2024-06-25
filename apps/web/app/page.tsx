7f914c56011bbec26d14c0d722dd3668174564a1
Neshant <neshant17@gmail.com>
2024-06-25 22:54:36 +0530

    first commit

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@repo/ui/button";
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <Button appName="Web App">
+           Hi there
+      </Button>
+      
+    </div>
+  );
+}
